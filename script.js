7ae41d9a8b611cc1dc399e4fe1d7b9dbec3e7e91
Simplejack17 <jack.tyburski1@gmail.com>
2017-10-23 21:04:08 -0400

    added a list of words and a function to generate a random word.

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-$(document).ready(function() {
-  console.log("hello world");
-});
+// $(document).ready(function() {
+//   console.log("hello world");
+// });
+
+var listOfWords = ["potato", "raindeer", "chair", "painting", "couch", "pants"];
+
+class Game {
+  constructor() {
+    this.answer = "";
+  }
+  generateWord() {
+    var randomNumber = Math.floor(Math.random() * listOfWords.length);
+    var chosenWord = listOfWords[randomNumber];
+    this.answer = chosenWord;
+  }
+
+  generateGameBoard() {
+    var numberOfSpaces = this.answer.length;
+    console.log(numberOfSpaces);
+  }
+}
+
+// function compareWord(string, userInputLetter) {
+//   console.log(string);
+// }
+// compareWord(letterInput);
+
+//step 1 need a function to generate answerWord function. = have it return a random word
+//step 2 a check letter function that checks the letter vs the answerWord
+//step 3 a check lettter function that we will put on each letter
+// create board function
+
+var newGame = new Game();
+
+newGame.generateWord();
+console.log(newGame.answer);
+
+newGame.generateGameBoard();
