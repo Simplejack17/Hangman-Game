d6d936f33413aa9031fb73b38cf8236c10873ce7
Simplejack17 <jack.tyburski1@gmail.com>
2017-10-26 15:04:35 -0400

    finished MVP's of game. Whole thing is functionable. Still a few bugs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hangman-Game
-My first project at General Assembly
+My first project at General Assembly - Jack Tyburski
+
+click a letter to guess your word!
+
+you only have 6 lives! so be careful!
--- script.js
+++ script.js
@@ -1,134 +1,149 @@
 $(document).ready(function() {
   //
-  var newGame = new Game(); //creates a new instance of a game
-  newGame.generateWord(); //generates a random word from our listOfWords array.
-  console.log(newGame.answer); //calls a new instance of our game with the generateWord in it
-  newGame.generateGameBoard(); //this is the empty squares that refer to the number of letters in this.answer
-  newGame.generateLetters(); // calls out make generateLetters function to create buttons
-});
+  var newGame = new Game() //creates a new instance of a game
+  newGame.generateWord() //generates a random word from our listOfWords array.
+  console.log(newGame.answer) //calls a new instance of our game with the generateWord in it
+  newGame.generateGameBoard() //this is the empty squares that refer to the number of letters in this.answer
+  newGame.generateLetters() // calls out make generateLetters function to create buttons
+})
 
 //random awnswer words
 var listOfWords = [
-  "potato",
-  "nature",
-  "halloween",
-  "mountain",
-  "couch",
-  "pants",
-  "turtle",
-  "football"
-];
+  'potato',
+  'nature',
+  'halloween',
+  'mountain',
+  'couch',
+  'pants',
+  'turtle',
+  'football'
+]
 //so i can compare letters.
-var chosenWord;
+var chosenWord
 //creates a new instance of a game
 class Game {
   constructor() {
-    this.answer = "";
-    this.currentGuess = "";
+    this.answer = ''
+    this.turnsRemaining = 7
+    this.correctLetters = 0
+    this.gameStatus = 'In progress'
   }
   //picking a random word out of the listOfWords
   generateWord() {
-    var randomNumber = Math.floor(Math.random() * listOfWords.length);
-    chosenWord = listOfWords[randomNumber];
-    this.answer = chosenWord;
+    var randomNumber = Math.floor(Math.random() * listOfWords.length)
+    chosenWord = listOfWords[randomNumber]
+    this.answer = chosenWord
   }
   //this is the empty squares that refer to the number of letters in this.answer
   generateGameBoard() {
-    var numberOfSpaces = this.answer.length;
-    console.log(numberOfSpaces);
+    var numberOfSpaces = this.answer.length
+    console.log(numberOfSpaces)
     //counting how many letters there are in this.answer and creating that many 'spaces'
     for (var i = 0; i < numberOfSpaces; i++) {
-      console.log("number of _ is working");
-      $(".gameboard").append(
-        '<span class="space">' + this.answer[i] + "</span>"
-      );
+      console.log('number of _ is working')
+      $('.gameboard').append(
+        '<span class="space"><span class="spaceLetter" data-letter="' +
+          this.answer[i] +
+          '">' +
+          this.answer[i] +
+          '</span></span>'
+      )
     }
+    $('.spaceLetter').addClass('hidden')
   }
   //puts the divs on the webpage and labeled
   generateLetters() {
     var alphabet = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z"
-    ];
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z'
+    ]
     //creates 26 divs containing the letters in the array above.
     for (var i = 0; i < alphabet.length; i++) {
-      console.log("letterContainer");
-      $(".letterSetContainer").append(
-        `<span class="letter" data-letter="${alphabet[i]}">` +
+      console.log('letterContainer')
+      $('.letterSetContainer').append(
+        `<button class="letter" data-letter="${alphabet[
+          i
+        ]}"><span class="innerLetter">` +
           alphabet[i] +
-          `</span>`
-      );
+          `</span></button>`
+      )
     }
+
     //gives the divs "clickability" and tells you what button you are clicking on. Will also return true or false if the letter is in "chosenWord"
-    $(".letterSetContainer").click(function(e) {
-      let letter = $(e.target).data("letter");
-      // console.log();
-      console.log(chosenWord.includes(letter));
-    });
+    $('.letter').click(e => {
+      let letter = $(e.target).data('letter')
+      $(this).attr('disabled', 'disabled')
+      $(this).css('background-color', 'red')
+      this.checkLetter(letter)
+    })
+  }
+
+  //this function compares the letter and if they are true... and if they are false they...
+  checkLetter(letter) {
+    let compareLetter = chosenWord.includes(letter)
+    console.log('Chosen letter: ' + letter)
+    if (compareLetter === true) {
+      var numCorrectLetters = $('.spaceLetter[data-letter="' + letter + '"]')
+        .length
+      this.correctLetters += numCorrectLetters
+
+      if (this.correctLetters === chosenWord.length) {
+        alert('game won!')
+        this.gameReset()
+      }
+      $('*[data-letter="' + letter + '"]').removeClass('hidden')
+    } else {
+      this.turnsRemaining -= 1
+      console.log('Turns Remaining:' + this.turnsRemaining)
+      this.drawBodyPart()
+    }
+  }
+  //draw a body part for every -1 turn remaining.
+  drawBodyPart() {
+    if (this.turnsRemaining === 6) {
+      $('#head').removeClass('hidden')
+    } else if (this.turnsRemaining === 5) {
+      $('#body').removeClass('hidden')
+    } else if (this.turnsRemaining === 4) {
+      $('#rightArm').removeClass('hidden')
+    } else if (this.turnsRemaining === 3) {
+      $('#leftArm').removeClass('hidden')
+    } else if (this.turnsRemaining === 2) {
+      $('#rightLeg').removeClass('hidden')
+    } else if (this.turnsRemaining === 1) {
+      $('#leftLeg').removeClass('hidden')
+      alert('You lost')
+      // this.gameReset()
+    } else {
+      alert('error. please refresh the page.')
+    }
+  }
+
+  gameReset() {
+    location.reload()
   }
 }
-//now that when i click on a letter it will return true or false,
-//we need to set the letters to .hide? and when we click the letter,
-// it will .show
-
-// frame1 = function() {
-//   draw(0, 150, 150, 150);
-// };
-//
-// frame2 = function() {
-//   draw(10, 0, 10, 600);
-// };
-//
-// frame3 = function() {
-//   draw(0, 5, 70, 5);
-// };
-//
-// frame4 = function() {
-//   draw(60, 5, 60, 15);
-// };
-//
-// torso = function() {
-//   draw(60, 36, 60, 70);
-// };
-//
-// rightArm = function() {
-//   draw(60, 46, 100, 50);
-// };
-//
-// leftArm = function() {
-//   draw(60, 46, 20, 50);
-// };
-//
-// rightLeg = function() {
-//   draw(60, 70, 100, 100);
-// };
-//
-// leftLeg = function() {
-//   draw(60, 70, 20, 100);
-// };
-
-//want the function to set this.letter to currentGuess.
